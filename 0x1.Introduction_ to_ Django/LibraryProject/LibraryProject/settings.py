562eedbc6b16219c0870edd7fba6fdc35f03a96b
Riunga <abbeyriunga12@gmail.com>
2024-08-13 13:22:14 +0300

    New changes

--- 0x1.Introduction_ to_ Django/LibraryProject/LibraryProject/settings.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/LibraryProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookshelf',
 ]
 
 MIDDLEWARE = [
@@ -121,8 +122,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-INSTALLED_APPS = [
-    # other apps
-    'bookshelf',
-]
