653cbc071877dcc7b87174826aefe82dcf4bbc9b
Riunga <abbeyriunga12@gmail.com>
2024-08-16 11:15:55 +0300

    Django Views and URL Configuration

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -20,3 +20,10 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+from django.urls import include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('relationship_app/', include('relationship_app.urls')),
+]
+
