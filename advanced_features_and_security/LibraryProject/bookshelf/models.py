5b444439401c34a1e0ac55dd938259e88c6a7951
Riunga <abbeyriunga12@gmail.com>
2024-08-23 19:48:19 +0300

    Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -52,4 +52,49 @@ class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
+# bookshelf/models.py
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Permission
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
+# bookshelf/models.py
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Permission
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
+
 
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -28,3 +28,59 @@ def delete_book(request, book_id):
         book.delete()
         return redirect('book_list')  # Redirect to a view that lists books
     return render(request, 'bookshelf/delete_book.html', {'book': book})
+
+# bookshelf/views.py
+
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from bookshelf.models import Book
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def view_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'bookshelf/view_book.html', {'book': book})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    # Your code for creating a book
+    pass
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    # Your code for editing the book
+    pass
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    # Your code for deleting the book
+    pass
+
+# bookshelf/views.py
+
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from bookshelf.models import Book
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def view_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'bookshelf/view_book.html', {'book': book})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    # Your code for creating a book
+    pass
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    # Your code for editing the book
+    pass
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    # Your code for deleting the book
+    pass
