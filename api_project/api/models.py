824858eeb57b29a0a5db503f124ffb680b7fd640
Riunga <abbeyriunga12@gmail.com>
2024-09-05 16:07:56 +0300

    Setting Up a New Django Project with Custom Serializers in Django REST Framework

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -7,3 +7,19 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)  # Author's name
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)  # Book's title
+    publication_year = models.IntegerField()  # Year of publication
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)  # Link to Author
+
+    def __str__(self):
+        return self.title
--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -5,3 +5,26 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['id', 'title', 'author']  # Include the fields you want to expose
+
+from rest_framework import serializers
+from .models import Author, Book
+import datetime
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['title', 'publication_year', 'author']
+
+    # Custom validation to ensure publication_year is not in the future
+    def validate_publication_year(self, value):
+        current_year = datetime.datetime.now().year
+        if value > current_year:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value
+
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)  # Nested serializer to include related books
+
+    class Meta:
+        model = Author
+        fields = ['name', 'books']
