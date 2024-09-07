db219f223f1a66de2f1feebefc8d893227722133
Riunga <abbeyriunga12@gmail.com>
2024-09-07 09:33:39 +0300

    "Refactor API tests: updated test cases, removed Author model, and changed API client and URLs"

--- api_project/api/test_views.py
+++ api_project/api/test_views.py
@@ -1,72 +1,57 @@
-from django.test import TestCase
-from rest_framework.test import APIClient
+import json
+from django.test import TestCase, Client
 from rest_framework import status
-from django.urls import reverse
-from .models import Book, Author
+from .models import Book
+from .serializers import BookSerializer
 
 class BookAPITestCase(TestCase):
-
     def setUp(self):
-        self.client = APIClient()
-        self.author = Author.objects.create(name="Author 1")
-        self.book_data = {
-            "title": "Book Title",
-            "publication_year": 2023,
-            "author": self.author.id
-        }
-        self.create_url = reverse('book-create')
+        self.client = Client()
+        self.book_data = {'title': 'Test Book', 'author': 'Test Author', 'publication_year': 2022}
 
     def test_create_book(self):
-        response = self.client.post(self.create_url, self.book_data, format='json')
+        response = self.client.post('/api/books/', self.book_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Book.objects.count(), 1)
-        self.assertEqual(Book.objects.get().title, 'Book Title')
-
-    def test_get_book_detail(self):
-        book = Book.objects.create(title="Sample Book", publication_year=2020, author=self.author)
-        response = self.client.get(reverse('book-detail', kwargs={'pk': book.id}))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['title'], 'Sample Book')
+        self.assertEqual(Book.objects.get().title, self.book_data['title'])
 
     def test_update_book(self):
-        book = Book.objects.create(title="Old Title", publication_year=2020, author=self.author)
-        updated_data = {
-            "title": "Updated Title",
-            "publication_year": 2021,
-            "author": self.author.id
-        }
-        response = self.client.put(reverse('book-update', kwargs={'pk': book.id}), updated_data, format='json')
+        book = Book.objects.create(**self.book_data)
+        updated_data = {'title': 'Updated Book'}
+        response = self.client.patch(f'/api/books/{book.id}/', updated_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        book.refresh_from_db()
-        self.assertEqual(book.title, "Updated Title")
+        self.assertEqual(Book.objects.get().title, updated_data['title'])
 
     def test_delete_book(self):
-        book = Book.objects.create(title="Sample Book", publication_year=2020, author=self.author)
-        response = self.client.delete(reverse('book-delete', kwargs={'pk': book.id}))
+        book = Book.objects.create(**self.book_data)
+        response = self.client.delete(f'/api/books/{book.id}/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Book.objects.count(), 0)
 
-    def test_filter_books(self):
-        author2 = Author.objects.create(name="Author 2")
-        Book.objects.create(title="Book 1", publication_year=2020, author=self.author)
-        Book.objects.create(title="Book 2", publication_year=2021, author=author2)
-
-        response = self.client.get(reverse('book-list'), {'author__name': 'Author 2'})
+    def test_list_books(self):
+        Book.objects.create(**self.book_data)
+        response = self.client.get('/api/books/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['title'], 'Book 2')
+
+    def test_filter_books(self):
+        Book.objects.create(**self.book_data)
+        response = self.client.get('/api/books/?title=Test Book')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
 
     def test_search_books(self):
-        Book.objects.create(title="Unique Title", publication_year=2020, author=self.author)
-        response = self.client.get(reverse('book-list'), {'search': 'Unique'})
+        Book.objects.create(**self.book_data)
+        response = self.client.get('/api/books/?search=Test')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['title'], 'Unique Title')
 
-    def test_order_books(self):
-        Book.objects.create(title="Book A", publication_year=2020, author=self.author)
-        Book.objects.create(title="Book B", publication_year=2021, author=self.author)
-
-        response = self.client.get(reverse('book-list'), {'ordering': 'title'})
+    def test_ordering_books(self):
+        Book.objects.create(**self.book_data)
+        response = self.client.get('/api/books/?ordering=title')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data[0]['title'], 'Book A')
+        self.assertEqual(len(response.data), 1)
+
+    def test_permissions(self):
+        # Test permission scenarios here
+        pass
