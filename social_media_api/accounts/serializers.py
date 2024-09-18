4fe0d4c51a6123e33377133266d86533e39832dd
Riunga <abbeyriunga12@gmail.com>
2024-09-18 10:01:03 +0300

    New changes

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,40 +1,24 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token
-from django.contrib.auth.password_validation import validate_password
 
-# Get the Custom User model
+# Get the custom user model
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    # Adding a write-only password field with validation
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)  # Password confirmation
-
     class Meta:
         model = User
-        fields = ['username', 'password', 'password2', 'bio', 'profile_picture']
+        fields = ['username', 'password', 'bio', 'profile_picture']
+        extra_kwargs = {'password': {'write_only': True}}
 
-    # Validate that password and password2 are the same
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-        return attrs
-
-    # Overriding the create method to handle user creation
+    # Overriding the create method to set the password correctly
     def create(self, validated_data):
-        # Remove password2 as we don't need it for user creation
-        validated_data.pop('password2')
-
-        # Creating the user with the provided username and password
-        user = User.objects.create_user(
+        user = User(
             username=validated_data['username'],
-            password=validated_data['password'],
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None)
         )
-
-        # Create a token for the newly created user
-        Token.objects.create(user=user)
-
+        # Use set_password to hash the password
+        user.set_password(validated_data['password'])
+        user.save()
         return user
+
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -6,14 +6,16 @@ from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 from rest_framework import status
 from .serializers import RegisterSerializer
-from django.contrib.auth import authenticate
 
 class RegisterView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            # Get the user's token after registration
-            token = Token.objects.get(user=user)
+
+            # Generate the token for the new user
+            token, created = Token.objects.get_or_create(user=user)
             return Response({'token': token.key}, status=status.HTTP_201_CREATED)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
