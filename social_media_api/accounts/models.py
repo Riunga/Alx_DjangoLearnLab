1c7cd9521b6a8e5c770b8ae27cfbeeb603eb1633
Riunga <abbeyriunga12@gmail.com>
2024-09-19 12:44:14 +0300

    Implementing Posts and Comments Functionality

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -6,3 +6,6 @@ class CustomUser(AbstractUser):
     bio = models.TextField(blank=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
     followers = models.ManyToManyField('self', blank=True, symmetrical=False)
+
+def __str__(self):
+        return self.username
